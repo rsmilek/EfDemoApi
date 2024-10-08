1b08a2739cad93cdfb9b91bddc690c7ee31a0fa4
Radim SMILEK <radim.smilek@cz.abb.com>
2024-10-08 15:43:48 +0200

    Added DB Domain structure very first implementation.

---
+++ EfDemo.Domain/EfDemoDbContext.cs
@@ -0,0 +1,23 @@
+﻿using EfDemo.SharedKernel.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EfDemo.Domain
+{
+    public class EfDemoDbContext : DbContext
+    {
+        private readonly IModelConfiguration modelConfiguration;
+
+        public EfDemoDbContext(DbContextOptions<EfDemoDbContext> options, IModelConfiguration modelConfiguration)
+            : base(options)
+        {
+            this.modelConfiguration = modelConfiguration;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //// modelBuilder.ApplyConfigurationsFromAssembly(typeof(EfDemoDbContext).Assembly);
+            modelConfiguration.ConfigureModel(modelBuilder);
+        }
+    }
+}
