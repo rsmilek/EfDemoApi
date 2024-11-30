63b6c82b12b66b3cf2a94047b5eed07aaf942e6d
Radim SMILEK <radim.smilek@cz.abb.com>
2024-11-30 18:14:17 +0100

    Added Author controller

---
+++ EfDemo.Api/Controllers/AuthorController.cs
@@ -0,0 +1,35 @@
+﻿using EfDemo.Api.DTOs;
+using EfDemo.Domain;
+using EfDemo.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfDemo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly EfDemoDbContext _context;
+
+        public AuthorController(EfDemoDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AuthorDTO>>> GetAuthors()
+        {
+            var authors = await _context.Set<Author>()
+                .Select(x => new AuthorDTO
+                {
+                    AuthorId = x.AuthorId,
+                    FirstName = x.FirstName,
+                    LastName = x.LastName
+                })
+                .ToListAsync();
+
+            return Ok(authors);
+        }
+    }
+}
---
+++ EfDemo.Api/DTOs/AuthorDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace EfDemo.Api.DTOs
+{
+    public class AuthorDTO
+    {
+        public int AuthorId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
